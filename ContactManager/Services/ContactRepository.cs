32f48c72cec80fc854e3d3d17ebab021daa82b48
Nodios <dinorepac93@gmail.com>
2016-04-20 12:18:18 +0200

    Read/write API

--- ContactManager/Services/ContactRepository.cs
+++ ContactManager/Services/ContactRepository.cs
@@ -8,31 +8,86 @@ namespace ContactManager.Services
 {
     public class ContactRepository
     {
+        private const string CacheKey = "ContactStore";
+
+        public ContactRepository()
+        {
+            var ctx = HttpContext.Current;
+
+            if (ctx != null)
+            {
+                if (ctx.Cache[CacheKey] == null)
+                {
+                    var contacts = new Contact[]
+                    {
+                        new Contact
+                        {
+                            Id = 1,
+                            Name = "Glenn Block"
+                        },
+                        new Contact
+                        {
+                            Id = 2,
+                            Name = "Dan Roth"
+                        },
+                        new Contact
+                        {
+                            Id = 3,
+                            Name = "Dino Repac"
+                        },
+                        new Contact
+                        {
+                            Id = 4,
+                            Name = "Davor Tomljenović"
+                        }
+                    };
+
+                    ctx.Cache[CacheKey] = contacts;
+                }
+            }
+        }
+
         public Contact[] GetAllContacts()
         {
+            var ctx = HttpContext.Current;
+
+            if (ctx != null)
+            {
+                return (Contact[])ctx.Cache[CacheKey];
+            }
+
             return new Contact[]
             {
-                 new Contact
-                 {
-                      Id = 1,
-                      Name = "Glenn Block"
-                 },
-                 new Contact
-                 {
-                      Id = 2,
-                      Name = "Dan Roth"
-                 },
-                 new Contact
-                 {
-                     Id = 3,
-                     Name = "Dino Repac"
-                 },
-                 new Contact
-                 {
-                     Id = 4,
-                     Name = "Davor Tomljenović"
-                 }
+                new Contact
+                {
+                    Id = 0,
+                    Name = "Placeholder"
+                }
             };
+        }
+
+        public bool SaveContact(Contact contact)
+        {
+            var ctx = HttpContext.Current;
+
+            if (ctx != null)
+            {
+                try
+                {
+                    var currentData = ((Contact[])ctx.Cache[CacheKey]).ToList();
+                    currentData.Add(contact);
+                    ctx.Cache[CacheKey] = currentData.ToArray();
+
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                    return false;
+                }
+            }
+
+            return false;
         }
     }
 }
